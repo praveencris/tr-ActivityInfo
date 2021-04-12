5401d80875a052a00c04daba793280d1873362ff
Praveen <pkchmits@gmail.com>
2021-04-12 08:33:06 +0530

    Added one way and two way databinding

--- app/src/main/java/com/example/activitylifecycledemo/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycledemo/MainActivity.kt
@@ -2,36 +2,65 @@ package com.example.activitylifecycledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import com.example.activitylifecycledemo.databinding.ActivityMainBinding
 
 const val TAG:String="MainActivity"
 
-lateinit var firstNameEdit:EditText
-lateinit var lastNameEdit:EditText
-lateinit var firstNameText:TextView
-lateinit var lastNameText:TextView
-lateinit var displayButton:Button
+
+lateinit var binding: ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity()/*, View.OnClickListener*/ {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //binding.lifecycleOwner=this
+        val name:Name=Name("First","Second")
+        binding.name=name
 
-        firstNameEdit=findViewById(R.id.firstNameEdit)
-        lastNameEdit=findViewById(R.id.lastNameEdit)
-        firstNameText=findViewById(R.id.firstNameText)
-        lastNameText=findViewById(R.id.lastNameText)
-        displayButton=findViewById(R.id.displayButton)
+      /*  binding.displayButton.setOnClickListener(View.OnClickListener {
+            binding.firstNameText.text = name.firstName
+            binding.lastNameText.text = name.lastName
+        })*/
 
+        binding.firstNameEdit.addTextChangedListener( object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
 
-        displayButton.setOnClickListener(View.OnClickListener {
-            firstNameText.text = firstNameEdit.text
-            lastNameText.text = lastNameEdit.text
+               binding.firstNameText.text=name.firstName
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
+
+        binding.lastNameEdit.addTextChangedListener( object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+                binding.lastNameText.text=name.lastName
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
         })
 
     }
@@ -71,3 +100,7 @@ class MainActivity : AppCompatActivity()/*, View.OnClickListener*/ {
         lastNameText.text = lastNameEdit.text
     }*/
 }
+
+
+
+data class Name(var firstName:String,var lastName:String)
